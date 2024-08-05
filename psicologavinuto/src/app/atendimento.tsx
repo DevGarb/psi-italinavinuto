564c275f915dfd2c3b87a947a1e44849a2baf50c
DevGarb <devgarb.fs@gmail.com>
2024-08-05 16:36:52 -0300

    fix:bug image importation

--- psicologavinuto/src/app/atendimento.tsx
+++ psicologavinuto/src/app/atendimento.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 import Consultorio from '../assets/img/consultoriopsicologia.png'
-import Wpp from '../assets/img/wpp.png'
+import Wpp from '../assets/img/Wpp.png'
 
 export default function Example() {
     return (
@@ -16,17 +16,13 @@ export default function Example() {
                             <Image className="object-cover w-full h-full" src={Consultorio} alt="Product"
                                 width={300}
                                 height={300} />
-                            {/* <div className="absolute inset-0 bg-black opacity-40"></div>
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                <button className="bg-white text-gray-900 py-2 px-6 rounded-full font-bold hover:bg-gray-300">View Product</button>
-                            </div> */}
+
                         </div>
                         <h3 className="text-xl font-bold mt-4">Psicóloga Idalina Vinuto: Atendimento Individual</h3>
                         <p className="text-sm mt-2">Como Psicóloga com mais de 10 anos de experiência atendendo adultos de 18 a 75 anos, sou Especialista em Burnout, auxilío no tratamento de depressão, ansiedade e estresse pós-traumático.</p>
                         <p className="text-sm mt-2"> Através da terapia trabalho para melhorar a relação entre pensamentos, emoções e comportamentos ajudando você a trilhar uma vida mais saudável e feliz.</p>
                         <div className="flex items-center justify-between mt-4">
                             <span className="font-bold text-lg flex gap-3"><Image className='object-contain' src={Wpp} alt='' />Vamos dar o primeiro passo</span>
-                            {/* <button className="bg-gray-900 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800">Add to Cart</button> */}
                         </div>
                     </div>
                     <div className="bg-[#FF7733] rounded-lg shadow-lg p-8">
@@ -34,17 +30,12 @@ export default function Example() {
                             <Image className="object-cover w-full h-full" src={Consultorio} alt="Product"
                                 width={300}
                                 height={300} />
-                            {/* <div className="absolute inset-0 bg-black opacity-40"></div>
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                <button className="bg-white text-gray-900 py-2 px-6 rounded-full font-bold hover:bg-gray-300">View Product</button>
-                            </div> */}
                         </div>
                         <h3 className="text-xl font-bold mt-4">Psicóloga Idalina Vinuto: Atendimento Individual</h3>
                         <p className="text-sm mt-2">Como Psicóloga com mais de 10 anos de experiência atendendo adultos de 18 a 75 anos, sou Especialista em Burnout, auxilío no tratamento de depressão, ansiedade e estresse pós-traumático.</p>
                         <p className="text-sm mt-2"> Através da terapia trabalho para melhorar a relação entre pensamentos, emoções e comportamentos ajudando você a trilhar uma vida mais saudável e feliz.</p>
                         <div className="flex items-center justify-between mt-4">
                             <span className="font-bold text-lg">Vamos dar o primeiro passo</span>
-                            {/* <button className="bg-gray-900 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800">Vamos dar o primeiro passo</button> */}
                         </div>
                     </div>
                 </div>

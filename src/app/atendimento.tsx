acc47c42e14280c07e622aeaeab340de1c8a90fc
Dev Garb <devgarb.fs@gmail.com>
2024-11-11 20:33:21 -0300

    fix:responsiviness bugs

--- src/app/atendimento.tsx
+++ src/app/atendimento.tsx
@@ -23,7 +23,7 @@ export default function Example() {
 
                 {/* Card de Atendimento Clínico */}
                 <div className="grid grid-cols-1 gap-8 m-10 mx-auto shadow-2xl rounded-lg overflow-hidden bg-[#F0E4DB] w-full">
-                    <div className="p-16 flex flex-col md:flex-row items-center gap-20 ">
+                    <div className="p-16 flex flex-col md:flex-row items-center gap-20 max-sm:gap-10 ">
 
                         {/* Texto e ícones */}
                         <div className="flex flex-col">
@@ -59,14 +59,12 @@ export default function Example() {
                 <div className="grid grid-cols-1 gap-8 m-10 mx-auto shadow-2xl rounded-lg overflow-hidden bg-[#E2D7CF] w-full">
                     <div className="p-16 flex flex-col lg:flex-row items-center gap-20 ">
                         {/* Imagem */}
-                        <div className="flex-shrink-0 rounded-md">
-                            <div>
-                                <Image
-                                    src={IdalinaMic}
-                                    alt="Psicóloga Idalina Vinuto"
-                                    className="rounded-lg object-cover"
-                                />
-                            </div>
+                        <div className="flex-shrink-0 rounded-md max-sm:hidden">
+                            <Image
+                                src={IdalinaMic}
+                                alt="Psicóloga Idalina Vinuto"
+                                className="rounded-lg object-cover"
+                            />
                         </div>
                         {/* Texto e ícones */}
                         <div className="flex flex-col">
@@ -79,7 +77,15 @@ export default function Example() {
                             <p className="text-gray-700 mb-4">
                                 Na minha trajetória profissional, fui responsável por implantar a área de Pessoas e Cultura em diversos segmentos, incluindo tecnologia, fintechs e varejo. Sempre com o objetivo de criar ambientes de trabalho saudáveis e produtivos, transformei cada desafio em uma oportunidade de crescimento e inovação.
                             </p>
-                            <p className="font-semibold flex items-center gap-2 text-[#945D38]">
+
+                            <div className="flex-shrink-0 rounded-md sm:hidden">
+                                <Image
+                                    src={IdalinaMic}
+                                    alt="Psicóloga Idalina Vinuto"
+                                    className="rounded-lg object-cover"
+                                />
+                            </div>
+                            <p className="max-sm:hidden font-semibold flex items-center gap-2 text-[#945D38]">
                                 <FaArrowDownLong className="text-[#945D38]" />
                                 Vamos dar o primeiro passo juntos?
                             </p>
--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 export default function footer() {
     return (
         <footer className="bg-[#241B0F] text-white py-8">
-            <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 font-playfair font-semibold ">
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 font-playfair">
                 <div className="border-t border-gray-500 pt-4 text-2xl tracking-wider">
-                    <h1 className="font-semibold">Instagram</h1>
+                    <h1 className="font-bold">Instagram</h1>
                 </div>
                 <div className="border-t border-gray-500 pt-4 text-2xl tracking-wider">
-                    <h1 className="font-semibold">LinkedIn</h1>
+                    <h1 className="font-bold">LinkedIn</h1>
                 </div>
                 <div className="border-t border-gray-500 pt-4 text-2xl tracking-wider">
-                    <h1 className="font-semibold">WhatsApp</h1>
+                    <h1 className="font-bold">WhatsApp</h1>
                 </div>
             </div>
             <div className="container mx-auto mt-28 flex flex-col md:flex-row justify-between text-center text-sm text-gray-400">
